2f062973445f8f8bef4f92ede9c17a368d6c12c3
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-03-17 17:25:40 +0530

    completed static components of skills component

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Skills = () => {
   return (
     <div>
-      <div className="bg-[#1b1e23]">
+      <div className="bg-[#1b1e23] py-10">
         <div className="headingFont text-5xl py-20 text-center">
           PROFESSIONAL SKILLS
         </div>
@@ -11,16 +11,73 @@ const Skills = () => {
           <div className="w-1/2">
             <div className="mx-20 my-10">
               <div className="headingFont text-3xl my-5 text-left mx-5">
-                HTML
+                HTML / CSS / JS
               </div>
+              <div className="text-right mr-10 mb-2 text-[#66dee5]">90%</div>
               <div className="w-full bg-black rounded-full">
-                <div className="bg-[#299e5c] text-xs font-medium text-blue-100 text-center p-0.25 rounded-full w-[90%]">
+                <div className="bg-[#66dee5] text-xs text-[#66dee5] font-bold text-center p-0.25 rounded-full w-[90%]">
                   90%
                 </div>
               </div>
             </div>
+            <div className="mx-20 my-10">
+              <div className="headingFont text-3xl my-5 text-left mx-5">
+                REACT JS
+              </div>
+              <div className="text-right mr-10 mb-2 text-[#66dee5]">80%</div>
+              <div className="w-full bg-black rounded-full">
+                <div className="bg-[#66dee5] text-xs text-[#66dee5] font-bold text-center p-0.25 rounded-full w-[80%]">
+                  80%
+                </div>
+              </div>
+            </div>
+            <div className="mx-20 my-10">
+              <div className="headingFont text-3xl my-5 text-left mx-5">
+                NODE JS
+              </div>
+              <div className="text-right mr-10 mb-2 text-[#66dee5]">80%</div>
+              <div className="w-full bg-black rounded-full">
+                <div className="bg-[#66dee5] text-xs text-[#66dee5] font-bold text-center p-0.25 rounded-full w-[80%]">
+                  80%
+                </div>
+              </div>
+            </div>
           </div>
-          <div>2nd</div>
+          <div className="w-1/2">
+            <div className="mx-20 my-10">
+              <div className="headingFont text-3xl my-5 text-left mx-5">
+                DJANGO
+              </div>
+              <div className="text-right mr-10 mb-2 text-[#66dee5]">70%</div>
+              <div className="w-full bg-black rounded-full">
+                <div className="bg-[#66dee5] text-xs text-[#66dee5] font-bold text-center p-0.25 rounded-full w-[70%]">
+                  70%
+                </div>
+              </div>
+            </div>
+            <div className="mx-20 my-10">
+              <div className="headingFont text-3xl my-5 text-left mx-5">
+                NETWORKING
+              </div>
+              <div className="text-right mr-10 mb-2 text-[#66dee5]">90%</div>
+              <div className="w-full bg-black rounded-full">
+                <div className="bg-[#66dee5] text-xs text-[#66dee5] font-bold text-center p-0.25 rounded-full w-[90%]">
+                  90%
+                </div>
+              </div>
+            </div>
+            <div className="mx-20 my-10">
+              <div className="headingFont text-3xl my-5 text-left mx-5">
+                KALI LINUX
+              </div>
+              <div className="text-right mr-10 mb-2 text-[#66dee5]">80%</div>
+              <div className="w-full bg-black rounded-full">
+                <div className="bg-[#66dee5] text-xs text-[#66dee5] font-bold text-center p-0.25 rounded-full w-[80%]">
+                  80%
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
