59c05d9786bf6c3fbbe7d2c83f6dbe3e1a146e45
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-03-28 21:49:08 +0530

    added animations to hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,12 +1,10 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import heroImg from "../assets/header_bg.jpg";
 import icons from "../constants/heroIcons";
 import { motion } from "framer-motion";
 import { AiFillCaretDown } from "react-icons/ai";
 
-
 const Hero = () => {
-
   return (
     <div>
       <div className="bg-gradient-to-r from-[#03897e] to-[#25b380] flex flex-row">
@@ -18,7 +16,7 @@ const Hero = () => {
               return (
                 <motion.div
                   animate={{ x: 0, y: 0 }}
-                  initial={{ x: -100, y: -100, opacity:0.6 }}
+                  initial={{ x: -100, y: -100, opacity: 0.6 }}
                   whileHover={{ scale: 1.5, opacity: 1 }}
                   drag={true}
                   whileTap={{ scale: 0.8, opacity: 1 }}
@@ -30,14 +28,173 @@ const Hero = () => {
               );
             })}
           </div>
-          <div className="text-white opacity-80 text-center mt-10">
-            <div className="text-8xl">
-              Hi There ! <br />
+          <div className="text-white text-center mt-10">
+            <div className="text-8xl flex w-full text-center pl-[35%]">
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+              >
+                H
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                
+              >
+                i
+              </motion.p>
+
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className="ml-10"
+              >
+                T
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+              >
+                h
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+              >
+                e
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+              >
+                r
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+              >
+                e
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className="ml-10"
+              >
+                !
+              </motion.p>
+              <br />
             </div>
-            <div className="text-4xl mt-4">
-              I am Lakshya <br />
+            <div className="flex w-full text-3xl gap-2 mt-5 pl-[35%]">
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className="ml-10"
+              >
+                I
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className="ml-5"
+              >
+                a
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                m
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className="ml-10"
+              >
+                L
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                a
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                k
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                s
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                h
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                y
+              </motion.p>
+              <motion.p
+                initial={{ scale: 0.2, x: -200 }}
+                whileTap={{ scale: 0.8, opacity: 1 }}
+                animate={{ scale: 1, x: 0, opacity: 0.6 }}
+                whileHover={{ scale: 1.5, opacity: 1 }}
+                className=""
+              >
+                a
+              </motion.p>
             </div>
-            <div className="mt-2">WEB DEVELOPER / ETHICAL HACKER</div>
+            <motion.div className="mt-2"
+              initial={{ opacity: 0 }} animate={{ opacity: 1, delay : 1 }}
+              >WEB DEVELOPER / ETHICAL HACKER</motion.div>
           </div>
           <div className="flex-row text-center my-10">
             <button className="m-10 border border-black py-2 px-10">
